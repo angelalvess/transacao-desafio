72d781a78d02db5a5244c2af6664138e70e46070
angelalvess <angeltrashangel@gmail.com>
2025-05-20 22:30:40 -0300

    feat: init project desafio-transacao

---
+++ src/main/java/com/angel/alves/transacao_desafio/TransacaoDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.angel.alves.transacao_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel/alves/transacao_desafio/TransacaoDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.alves.transacao_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
